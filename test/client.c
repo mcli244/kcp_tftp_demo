e0aaee963b48e9f1f331dac502d97d744e6f53e7
CoreyLee <lixiangjun@up3dtech.com>
2024-10-11 20:22:17 -0700

    [feat]

--- test/client.c
+++ test/client.c
@@ -8,25 +8,40 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <errno.h>
+#include <pthread.h>
 
 #include "test.h"
+#include "up3d.h"
 
 #define SERVER_IP   "127.0.0.1"
 #define SERVER_PORT 8888
 #define CLIENT_PORT 8001
 
 #define KCP_CONV 12345
-struct sockaddr_in server_addr;
-ikcpcb *kcp;
 
-int make_socket_non_blocking(int sockfd)
+
+typedef struct
 {
-    int flags = fcntl(sockfd, F_GETFL, 0);
+    int sfd;
+    int sys_runing;
+    struct sockaddr_in server_addr;
+    pthread_t recv_pid;
+    pthread_t send_pid;
+    ikcpcb *kcp;
+    char *file_name;
+}UP3D_Client_Ctx_Type;
+
+UP3D_Client_Ctx_Type cctx;
+
+
+int set_nonblocking(int fd)
+{
+    int flags = fcntl(fd, F_GETFL, 0);
     if (flags == -1) {
         return -1;
     }
     flags |= O_NONBLOCK;
-    if (fcntl(sockfd, F_SETFL, flags) == -1) {
+    if (fcntl(fd, F_SETFL, flags) == -1) {
         return -1;
     }
     return 0;
@@ -36,82 +51,29 @@ int udp_output(const char *buf, int len, ikcpcb *kcp, void *user)
 {
     static int cnt = 0;
     static int index = 0;
-	int sockfd = *(int *)user;
-    int client_len = sizeof(struct sockaddr_in);
+    int slen = sizeof(struct sockaddr_in);
+    UP3D_Client_Ctx_Type *cctx = (UP3D_Client_Ctx_Type *)user;
 
     cnt += len;
-    printf("%02d: udp_output len:%d cnt:%d\n", index++, len, cnt);
-    return sendto(sockfd, buf, len, 0, (struct sockaddr*)&server_addr, client_len);
+    _LOG(_LOG_DEBUG, "%02d: udp_output len:%d cnt:%d\n", index++, len, cnt);
+    return sendto(cctx->sfd, buf, len, 0, (struct sockaddr*)&cctx->server_addr, slen);
 }
 
-// 100ms
-void timer_handler(int signum) {
-    // printf("timer_handler\n");
-    ikcp_update(kcp, iclock());
-}
-
-
-int init_timer(void)
+int socket_creat(char *s_ip, int s_port)
 {
-    struct sigaction sa;
-    struct itimerspec timer_spec;
-    timer_t timerid;
+    int sockfd;
 
-    // 设置定时器信号处理函数
-    sa.sa_flags = SA_SIGINFO;
-    sa.sa_handler = timer_handler;
-    sigemptyset(&sa.sa_mask);
-    sigaction(SIGALRM, &sa, NULL);
-
-    // 创建POSIX定时器
-    if (timer_create(CLOCK_REALTIME, NULL, &timerid) == -1) {
-        perror("timer_create");
-        return -1;
-    }
-
-    // 定时器的初始时间和间隔时间设置
-    timer_spec.it_value.tv_sec = 0;  // 初始延时1秒
-    timer_spec.it_value.tv_nsec = 100*1000*1000;
-    timer_spec.it_interval.tv_sec = 0;  // 间隔时间2秒
-    timer_spec.it_interval.tv_nsec = 100*1000*1000;
-
-    // 设置定时器
-    if (timer_settime(timerid, 0, &timer_spec, NULL) == -1) {
-        perror("timer_settime");
-        return -1;
-    }
-
-    return 0;
-}
-
-int main(int argc, char **argv)
-{
-    
-    int sockfd, len, maxfd, nready;
-    uint32_t conv = KCP_CONV; // 这里需要和服务端保持一致
-    fd_set rset, allset;
-    
-
-    printf("test client\n");
-
-    if(argc != 4)
-    {
-        printf("Usage: %s ip port filename\n", argv[0]);
-        exit(EXIT_FAILURE);
-    }
-
-    // 创建 UDP 套接字
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
     if (sockfd < 0) {
         perror("socket error");
         exit(EXIT_FAILURE);
     }
 
-    printf("server:%s:%d\n", argv[1], atoi(argv[2]));
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = inet_addr(argv[1]);
-    server_addr.sin_port = htons(atoi(argv[2]));
+    printf("server:%s:%d\n", s_ip, s_port);
+    memset(&cctx.server_addr, 0, sizeof(cctx.server_addr));
+    cctx.server_addr.sin_family = AF_INET;
+    cctx.server_addr.sin_addr.s_addr = inet_addr(s_ip);
+    cctx.server_addr.sin_port = htons(s_port);
 
     // memset(&local_addr, 0, sizeof(local_addr));
     // local_addr.sin_family = AF_INET;
@@ -127,38 +89,70 @@ int main(int argc, char **argv)
     //     exit(EXIT_FAILURE);
     // }
 
-    // 初始化 KCP 协议控制块
-    kcp = ikcp_create(conv, (void *)&sockfd);
-    kcp->output = udp_output;
-    // 将 KCP 协议控制块设为快速模式，以提高传输速度
-    ikcp_nodelay(kcp, 1, 10, 2, 1);
-    
-    ikcp_update(kcp, 0);
-    // init_timer();
+    return sockfd;
+}
 
-    uint8_t buf[1025];
-    int n = 0;
-    int pyload_cnt = 0;
-
+void *recv_process(void *arg)
+{
+    int len, remote_len, ret, n;
+    uint8_t buf[1024];
+    struct sockaddr_in remote_addr;
     
-    int kcp_raw_cnt = 0;
-    int ret = 0;
-    int send_cnt = 0;
+    UP3D_Client_Ctx_Type *cctx = (UP3D_Client_Ctx_Type *)arg;
+    remote_len = sizeof(struct sockaddr_in);
+    
+    _LOG(_LOG_INFO, "recv_process start");
+    
+    while(cctx->sys_runing)
+    {
+        ikcp_update(cctx->kcp, iclock());
+        usleep(10*1000);
+        len = recvfrom(cctx->sfd, buf, sizeof(buf), MSG_DONTWAIT, (struct sockaddr*)&remote_addr, &remote_len);
+        if (len <= 0) {
+            continue;
+        } else {
+            _LOG(_LOG_DEBUG, "len:%d", len);
+            ret = ikcp_input(cctx->kcp, buf, len);
+            if(ret < 0) continue;// 有可能是控制报文，或者ACK等
+            while(1)    // 真正数据
+            {
+                n = ikcp_recv(cctx->kcp, buf, sizeof(buf));
+                if (n < 0) {
+                    break;
+                } else {
+                    _LOG(_LOG_DEBUG, "received data from client: %d bytes ret:%d \n", n, ret);
+                }
+            }
+        }
+    }
+
+    _LOG(_LOG_INFO, "recv_process exit");
+}
+
+void *send_process(void *arg)
+{
+    int len, ret, n;
+    uint8_t buf[1024];
+    char msg[256];
+    int pyload_cnt = 0, kcp_raw_cnt = 0, send_cnt = 0;
+    UP3D_Client_Ctx_Type *cctx = (UP3D_Client_Ctx_Type *)arg;
+
 
     FILE *file;
-    if ((file = fopen(argv[3], "r")) == NULL)
-        goto err;
+    if ((file = fopen(cctx->file_name, "r")) == NULL)
+    {
+        _LOG(_LOG_ERR, "fopen failed %s", cctx->file_name);
+        return NULL;
+    }
 
-    char msg[256];
+    set_nonblocking(STDIN_FILENO);
 
-    int flags = fcntl(STDIN_FILENO, F_GETFL, 0);
-    flags |= O_NONBLOCK;
-    fcntl(STDIN_FILENO, F_SETFL, flags);
-    while(1)
+    _LOG(_LOG_INFO, "send_process start");
+    while(cctx->sys_runing)
     {
         if(fgets(msg , sizeof(msg) , stdin) != NULL)
         {
-            printf("msg:[%s]\n", msg);
+            _LOG(_LOG_INFO, "msg:[%s]\n", msg);
             if(strcmp(msg , "send\n") == 0)
             {
                 fseek(file, 0, SEEK_SET);
@@ -166,30 +160,75 @@ int main(int argc, char **argv)
                 send_cnt = 0;
                 while (1) {
                     n = fread(buf, 1, sizeof(buf), file);
-                    if(n <= 0)
-                        break;
-                    ret = ikcp_send(kcp, buf, n);
-                    // printf("ikcp_send ret:%d\n", ret);
-                    ikcp_flush(kcp);
+                    if(n <= 0)  break;
+                    ret = ikcp_send(cctx->kcp, buf, n);
                     pyload_cnt += n;
                     send_cnt ++;
                 }
-                printf("n:%d ret:%d send_cnt:%d pyload_cnt:%d \n", n, ret, send_cnt, pyload_cnt);
+                _LOG(_LOG_INFO, "n:%d ret:%d send_cnt:%d pyload_cnt:%d \n", n, ret, send_cnt, pyload_cnt);
             }
             else if(strcmp(msg , "q\n") == 0)
             {
+                cctx->sys_runing = 0;
                 break;
             }
         }
-        ikcp_update(kcp, 0);
         usleep(100*1000);
     }
-    
-    fclose(file);
 
+    fclose(file);
+    _LOG(_LOG_INFO, "send_process exit");
+}
+
+int main(int argc, char **argv)
+{
+    
+    int ret, len;
+    uint32_t conv = KCP_CONV; // 这里需要和服务端保持一致
+
+    _LOG(_LOG_INFO, "test client\n");
+    if(argc != 4)
+    {
+        _LOG(_LOG_ERR, "Usage: %s ip port filename\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    cctx.sys_runing = 1;
+    cctx.file_name = argv[3];
+    cctx.sfd = socket_creat(argv[1], atoi(argv[2]));
+    
+    cctx.kcp = ikcp_create(conv, (void *)&cctx);
+    cctx.kcp->output = udp_output;
+    ikcp_nodelay(cctx.kcp, 1, 10, 2, 1);
+    ikcp_update(cctx.kcp, 0);   // 必须要调这个
+    
+    ret = pthread_create(&cctx.recv_pid , NULL , recv_process, (void *)&cctx);
+    if(0 != ret)
+    {
+        _LOG(_LOG_ERR, "pthread_create failed");
+        perror("pthread_create");
+        goto err;
+    }
+
+    ret = pthread_create(&cctx.send_pid , NULL , send_process, (void *)&cctx);
+    if(0 != ret)
+    {
+        _LOG(_LOG_ERR, "pthread_create failed");
+        perror("pthread_create");
+        goto err;
+    }
+
+    pthread_detach(cctx.recv_pid);
+    pthread_detach(cctx.send_pid);
+
+    while(cctx.sys_runing)
+    {
+        sleep(3);
+    }
+    
 err:
-    ikcp_release(kcp);
-    close(sockfd);
-    printf("exit\n");
+    ikcp_release(cctx.kcp);
+    close(cctx.sfd);
+    _LOG(_LOG_INFO, "exit\n");
     return 0;
 }
